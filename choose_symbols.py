198332e88d15509715d1e9e35bf184ccd35ce52a
JingX1079 <70962002+JingX1079@users.noreply.github.com>
2020-11-21 20:47:35 +0800

    Update choose_symbols.py
    Added logging statements

--- choose_symbols.py
+++ choose_symbols.py
@@ -1,8 +1,12 @@
 from Player import Player
 from ask_symbol import ask_symbol
+import logging
+import logging.handlers
+import os
+from datetime import datetime
 
 def choose_symbols(p1, p2):
-
+    log = logging.getLogger("main")
     # If p1 is first, they choose symbol first
     if p1.get_turn() == "first":
 
@@ -12,11 +16,14 @@ def choose_symbols(p1, p2):
         if p1_symb == "x":
             p1.set_symbol("x")
             p2.set_symbol("o")
+            log.info(p1.get_name()+"  has symbol  "+p1.get_symbol().upper()+"  AND  "+p2.get_name()+"  has symbol  "+p2.get_symbol().upper())
+            
 
         # If p1 chooses 'o', p2 gets 'x'
         if p1_symb == "o":
             p1.set_symbol("o")
             p2.set_symbol("x")
+            log.info(p1.get_name()+"has symbol"+p1.get_symbol().upper()+"  AND  "+p2.get_name()+"has symbol"+p2.get_symbol().upper())
 
     # Otherwise, p2 chooses symbol first
     else:
@@ -26,11 +33,14 @@ def choose_symbols(p1, p2):
         if p2_symb == "x":
             p1.set_symbol("o")
             p2.set_symbol("x")
+            log.info(p1.get_name()+"has symbol"+p1.get_symbol().upper()+"  AND  "+p2.get_name()+"has symbol"+p2.get_symbol().upper())
 
         # If p2 chooses 'o', p1 gets 'x'
         if p2_symb == "o":
             p1.set_symbol("x")
             p2.set_symbol("o")
+            log.info(p1.get_name()+"has symbol"+p1.get_symbol().upper()+"  AND  "+p2.get_name()+"has symbol"+p2.get_symbol().upper())
 
     # Prints users' symbols for clarification
     print("\n" + p1.get_name() + " is '" + p1.get_symbol().upper() + "' and " + p2.get_name() + " will be '" + p2.get_symbol().upper() + "'")
+
