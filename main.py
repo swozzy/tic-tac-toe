be81ede9ee263a03ebbcff8e0785291f47bf199c
JingX1079 <70962002+JingX1079@users.noreply.github.com>
2020-11-21 20:42:51 +0800

    Update main.py
    Added logging library

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from initialization import initialization
 from play_again import play_again
+import logging
+import logging.handlers
+import os
+from datetime import datetime
 
 def main():
+
+    log = logging.getLogger("main")
+    logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%Y:%m:%d %I:%M:%S %p')
+    logging.basicConfig(level=os.environ.get("LOGLEVEL","INFO"))
+    handler = logging.handlers.WatchedFileHandler(os.environ.get("LOGFILE","app-log.log"))
+    handler.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s - %(funcName)s - line %(lineno)d"))
+    log.addHandler(handler)
+    log.setLevel(logging.INFO)
+    log.info("Game start")
+
+
 
     initialization()
 
