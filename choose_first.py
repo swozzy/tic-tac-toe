c672db002d0e315c3a24c38ea3546f085e62ce03
JingX1079 <70962002+JingX1079@users.noreply.github.com>
2020-11-21 20:44:33 +0800

    Update choose_first.py
    added logging statement

--- choose_first.py
+++ choose_first.py
@@ -1,10 +1,21 @@
 # Imports
 from Player import Player
 from random import randint
+import logging
+import logging.handlers
+import os
+from datetime import datetime
 
 # Sets either Player 1 or Player 2 be first
 # The player not chosen first goes second
 def choose_first(p1, p2):
+
+    log = logging.getLogger("main")
+  #  logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%Y:%m:%d %I:%M:%S %p')
+  #  handler = logging.handlers.WatchedFileHandler(os.environ.get("LOGFILE","app-log.log"))
+   # handler.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s - %(funcName)s - line %(lineno)d"))
+   # log.addHandler(handler)
+   # log.setLevel(logging.INFO)
 
     rand = randint(0, 1)
 
@@ -20,5 +31,9 @@ def choose_first(p1, p2):
     # Prints who goes first for user clarification
     if p1.get_turn() == "first":
         print("** " + p1.get_name() + " will go first and " + p2.get_name() + " will go second **")
-    if p2.get_turn() == "first":
+        log.info("** " + p1.get_name() + " will go first and " + p2.get_name() + " will go second **222")
+    #elif p2.get_turn() == "first":
+    else:
         print("** " + p2.get_name() + " will go first and " + p1.get_name() + " will go second **")
+        log.info("** " + p2.get_name() + " will go first and " + p1.get_name() + " will go second **111")
+
